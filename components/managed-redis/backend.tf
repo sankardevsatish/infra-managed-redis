047bf29cfedb19fc1f17997cce3549f95ce699d6
sankar <sankar>
2026-03-05 10:28:46 +0000

    added backend for storage

--- components/managed-redis/backend.tf
+++ components/managed-redis/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-dev-platform"
+    storage_account_name = "terraformstatefileredis"
+    container_name       = "tfstate"
+    key                  = "managed-redis.tfstate"
+  }
 }
